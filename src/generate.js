d9e21cba8c512a92e2b0014a880611589e897dde
Ahmed Barhoumi <banoni.bro@gmail.com>
2024-07-31 17:31:30 +0100

    chore: remove Makefile and migrate PDF generation to JavaScript for better flexibility and maintainability feat(generate.js): enhance PDF generation logic by adding timestamped filenames and cleaning up auxiliary files feat(temp1.ejs): update LaTeX template to include additional formatting and contact information fields

--- src/generate.js
+++ src/generate.js
@@ -9,18 +9,13 @@ async function generatePdf(data) {
   const templatePath = path.join(__dirname, "../templates/temp1.ejs");
   const templateSource = fs.readFileSync(templatePath, "utf8");
 
-  // Log the data and template for debugging
-  console.log("Template Source:", templateSource);
-  console.log("Data:", data);
-
   try {
-    // Ensure data is defined and has the expected fields
-    if (!data || typeof data !== "object") {
-      throw new Error("Invalid data format");
-    }
-
+    // Render the template with the provided data
     const output = ejs.render(templateSource, data);
-    const outputTexPath = path.join(__dirname, "../generated_resume.tex");
+    const outputTexPath = path.join(
+      __dirname,
+      "../templates/generated_resume.tex",
+    );
     fs.writeFileSync(outputTexPath, output);
 
     const pdfDir = path.join(__dirname, "../pdfs");
@@ -28,21 +23,33 @@ async function generatePdf(data) {
       fs.mkdirSync(pdfDir);
     }
 
-    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+    // Generate a timestamp for the PDF filename
+    const now = new Date();
+    const timestamp =
+      now.toISOString().split("T")[0] + // Get the date part (YYYY-MM-DD)
+      "-" +
+      now.getHours().toString().padStart(2, "0") +
+      "h" + // Get the hour (HH) and add 'h'
+      now.getMinutes().toString().padStart(2, "0") +
+      "m"; // Get the minute (MM) and add 'm'
     const pdfName = `ahmed-barhoumi-resume-${timestamp}.pdf`;
     const pdfPath = path.join(pdfDir, pdfName);
-
+    // Run pdflatex to generate the PDF
     await execAsync(`pdflatex -output-directory=${pdfDir} ${outputTexPath}`);
 
+    // Rename the generated PDF to the desired name
     fs.renameSync(path.join(pdfDir, "generated_resume.pdf"), pdfPath);
 
-    // Clean up auxiliary files
+    // Clean up auxiliary files in the pdfs directory
     ["aux", "log", "out"].forEach((ext) => {
-      const auxPath = outputTexPath.replace(".tex", `.${ext}`);
+      const auxPath = path.join(pdfDir, `generated_resume.${ext}`);
       if (fs.existsSync(auxPath)) {
         fs.unlinkSync(auxPath);
       }
     });
+
+    // Clean up the .tex file
+    fs.unlinkSync(outputTexPath);
 
     return pdfPath;
   } catch (error) {
